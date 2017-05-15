57b458094ee7d957bfc2a08d62865f98f052af6f
Luís Fernandes <luisapidcloud@hotmail.com>
2017-05-15 12:24:00 +0100

    Fix npm scripts for windows

--- test/core/cross-env_test.ts
+++ test/core/cross-env_test.ts
@@ -2,7 +2,8 @@
  * Created by luisf on 15/05/2017.
  */
 
-const opts = process.env.TS_NODE_COMPILER_OPTIONS;
-console.log(opts);
-console.log(typeof opts);
-console.log('Try parse', JSON.parse(opts));
+const e = process.env.TS_NODE_COMPILER_OPTIONS;
+console.log(e);
+console.log(typeof e);
+console.log('Try parse', JSON.parse(e));
+console.log('normal', JSON.parse('{"module":"commonjs"}'));
