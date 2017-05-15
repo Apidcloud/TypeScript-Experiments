026e6b2ef429d05d1e5b1715821b5800494e69f6
Luís Fernandes <luisapidcloud@hotmail.com>
2017-05-15 03:00:18 +0100

    Working unit testing sample with mocha

--- test/core/animal_test.ts
+++ test/core/animal_test.ts
@@ -2,6 +2,9 @@
  * Created by luisf on 15/05/2017.
  */
 
+import { expect } from 'chai';
+import 'mocha';
+
 import Animal from '../../src/core/animal';
 
 describe("Animal suite", () => {
@@ -10,10 +13,12 @@ describe("Animal suite", () => {
     let input = "sample input";
     let expectedResult = "sample input";
 
+
     it("returns the input", () =>{
 
         animal = new Animal("donkey");
-        expect(animal.simpleReturn(input)).toBe(expectedResult);
+        const result = animal.simpleReturn(input);
+        expect(result).to.equal(expectedResult);
     })
 
 });
