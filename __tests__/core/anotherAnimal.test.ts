e0d50d8b2ab7e46d2c6fd49b4cf3612e60a1db99
Luís Fernandes <luisapidcloud@hotmail.com>
2017-05-26 17:59:59 +0100

    Add missing test to anotherAnimal class

---
+++ __tests__/core/anotherAnimal.test.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by luisf on 26/05/2017.
+ */
+
+/**
+ * Created by luisf on 15/05/2017.
+ */
+
+import AnotherAnimal from '../../src/core/anotherAnimal';
+
+test("Another animal suite", () => {
+
+    let animal;
+
+    animal = new AnotherAnimal();
+
+    animal.anotherMethod();
+
+});
