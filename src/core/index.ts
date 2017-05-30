5a5817cc5912275794838070556d4b3d8dc31ddd
Luís Fernandes <luisapidcloud@hotmail.com>
2017-05-31 00:17:52 +0100

    chore(webpack): Add missing index files after the rename

---
+++ src/core/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by luisf on 11/05/2017.
+ */
+
+export { default as Animal } from './animal';
+
+export { default as Horse } from './horse';
+
+export { default as Snake, TESTSYMBOL } from './snake';
+
+export { default as AnotherAnimal } from './anotherAnimal';
